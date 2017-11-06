aaad8b3a792fc6d675f17a033e2975ae2d1753e7
Amanda K <kosateam@yahoo.com>
2017-11-06 03:38:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,3 +27,11 @@ function appendKitten(name){
 function prependKitten(name){
   return [name, ...kittens]
 }
+
+function removeLastKitten(){
+  
+}
+
+function removeFirstKitten(){
+  return kitten.slice(1)
+}
